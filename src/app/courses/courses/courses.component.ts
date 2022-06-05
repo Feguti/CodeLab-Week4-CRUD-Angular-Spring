8d1024ade2378c9246364837ba6a0f0d6aafccde
Felipe Gutierrez <feguti4@gmail.com>
2022-06-05 18:46:40 -0300

    Creating Spinner for course's list

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from "../model/course";
-import {CoursesService} from "../services/courses.service";
-import {Observable} from "rxjs";
+import { CoursesService } from "../services/courses.service";
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-courses',
@@ -10,12 +10,12 @@ import {Observable} from "rxjs";
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   constructor(private coursesService: CoursesService) {
     // this.courses = [];
-    this.courses = this.coursesService.listAll();
+    this.courses$ = this.coursesService.listAll();
   }
 
   ngOnInit(): void {
