e8a93bed2f500198e3885f9d0f4da3ca07d92a69
Felipe Gutierrez <feguti4@gmail.com>
2022-06-05 17:36:03 -0300

    Implementing Material Table CSS and creating App Material Module

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -9,7 +9,7 @@ import { Course } from "../model/course";
 export class CoursesComponent implements OnInit {
 
   courses: Course[] = [
-    {_id: '1', name: 'IT', category: 'Technical'}
+    {_id: '1', name: 'Information Technology', category: 'Technical'}
   ];
   displayedColumns = ['name', 'category'];
 
